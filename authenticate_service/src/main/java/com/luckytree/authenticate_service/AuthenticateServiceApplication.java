78f6352c348dce9df54bd7c5e7531ecb725a147f
gowjr207@gmail.com <gowjr207@gmail.com>
2022-12-21 00:54:21 +0900

    [STYLE] eureka client enable

--- authenticate_service/src/main/java/com/luckytree/authenticate_service/AuthenticateServiceApplication.java
+++ authenticate_service/src/main/java/com/luckytree/authenticate_service/AuthenticateServiceApplication.java
@@ -2,7 +2,9 @@ package com.luckytree.authenticate_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthenticateServiceApplication {
 
